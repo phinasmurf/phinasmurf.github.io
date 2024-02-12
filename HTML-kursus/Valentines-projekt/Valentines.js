caef0183f458789b014614b36a923906c330f7ec
phinasmurf <147978759+phinasmurf@users.noreply.github.com>
2024-02-12 14:32:15 +0100

    Yes and no button alerts - Valentines

--- HTML-kursus/Valentines-projekt/Valentines.js
+++ HTML-kursus/Valentines-projekt/Valentines.js
@@ -1,8 +1,18 @@
-const yesbutton = document.getElementById("")
-
+//If yes button is pressed
 function pressYes(){
     if (5 == 5) {
         alert("Good choice >:)")
+    }
+
+    else{
+        alert("Awh..")
+    }
+}
+
+//If no button is pressed
+function pressNo(){
+    if (5 == 5) {
+        alert("Wrong choice.. ")
     }
 
     else{
