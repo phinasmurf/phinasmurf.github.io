7dc513a891e0c63780ee1b698528d718b671beb8
phinasmurf <147978759+phinasmurf@users.noreply.github.com>
2024-02-21 08:23:48 +0100

    Changing text commit

--- HTML-kursus/click me/ClickMe.js
+++ HTML-kursus/click me/ClickMe.js
@@ -1,3 +1,24 @@
+/* function buttonpressed() {
+   var width = Math.random() * window.innerWidth;
+   var length = Math.random() * window.innerlength;
+
+   this.style.left = width + "px";
+   this.style.top = length + "px";
+} 
+
+
+// Functions for different texts in the button
+let textlist = ["Dont click me ", "seriously", "That hurts!", "STOP IT!!", "Why are you doing this?",];
+
 function buttonpressed() {
-   var 
+   var thebutton = document.getElementById("clickmebtn");
+   thebutton.innerHTML = textlist[Math.floor(Math.random() * textlist.length)];
+   console.log(thebutton.innerHTML);
+} */
+
+
+// Path: HTML-kursus/click%20me/ClickMe.js
+
+function buttonpressed() {
+   
 }
