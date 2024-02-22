e8ecaa2c9fdff1713247f1ed7b3bfb56e2f7f404
phinasmurf <147978759+phinasmurf@users.noreply.github.com>
2024-02-22 09:06:55 +0100

    Click me commit

--- HTML-kursus/click me/ClickMe.js
+++ HTML-kursus/click me/ClickMe.js
@@ -1,9 +1,5 @@
-/* function buttonpressed() {
-   var width = Math.random() * window.innerWidth;
-   var length = Math.random() * window.innerlength;
-
-   this.style.left = width + "px";
-   this.style.top = length + "px";
+function buttonpressed() {
+   
 } 
 
 
@@ -14,11 +10,15 @@ function buttonpressed() {
    var thebutton = document.getElementById("clickmebtn");
    thebutton.innerHTML = textlist[Math.floor(Math.random() * textlist.length)];
    console.log(thebutton.innerHTML);
-} */
+
+   var width = Math.random() * window.innerWidth;
+   var length = Math.random() * window.innerlength;
+
+   this.style.left = width + "px";
+   this.style.top = length + "px";
+
+} 
 
 
 // Path: HTML-kursus/click%20me/ClickMe.js
 
-function buttonpressed() {
-   
-}
