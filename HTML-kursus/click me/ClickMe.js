9195180fda49813216ab020af46b0eb69daad0ce
phinasmurf <147978759+phinasmurf@users.noreply.github.com>
2024-02-26 09:05:29 +0100

    BUTTON COMMIT

--- HTML-kursus/click me/ClickMe.js
+++ HTML-kursus/click me/ClickMe.js
@@ -1,24 +1,25 @@
-function buttonpressed() {
-   
-} 
-
-
 // Functions for different texts in the button
 let textlist = ["Dont click me ", "seriously", "That hurts!", "STOP IT!!", "Why are you doing this?",];
 
-function buttonpressed() {
+function buttonpressed(x) {
+   let lastOutput = "";
    var thebutton = document.getElementById("clickmebtn");
-   thebutton.innerHTML = textlist[Math.floor(Math.random() * textlist.length)];
-   console.log(thebutton.innerHTML);
 
-   var width = Math.random() * window.innerWidth;
-   var length = Math.random() * window.innerlength;
+   if (x !== lastOutput) {
+      thebutton.innerHTML = textlist[Math.floor(Math.random(x) * textlist.length)];
+      console.log(thebutton.innerHTML);
+      lastOutput = x;
+   } else{
+      console.log("Duplicate");
+   }
+
+
+   // var width = Math.random() * window.innerWidth;
+   // var length = Math.random() * window.innerlength;
 
    this.style.left = width + "px";
    this.style.top = length + "px";
 
 } 
 
-
-// Path: HTML-kursus/click%20me/ClickMe.js
 
